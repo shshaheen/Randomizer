54e8e647f0f04703d14ec112720a14a526589f61
shshaheen <kolimishaheen9885@gmail.com>
2025-01-11 11:18:15 +0530

    Made some brute-force trials

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:randomizer/style_text.dart';
+import 'dart:math';
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   final List<Color> colors;
-  const GradientContainer({super.key,
-  required this.colors});
+  const GradientContainer({super.key, required this.colors});
+
+  @override
+  State<GradientContainer> createState() => _GradientContainerState();
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+  final List<String> images = [
+    'assets/images/dice-1.png',
+    'assets/images/dice-2.png',
+    'assets/images/dice-3.png',
+    'assets/images/dice-4.png',
+    'assets/images/dice-5.png',
+    'assets/images/dice-6.png',
+  ];
+
+  int generateRandom() {
+    return Random().nextInt(6);
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(generateRandom());
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors:colors,
+          colors: widget.colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        
-        child: StyleText(text: "Hello Developer..!"),
+      child: Column(
+        children: [
+          Image.asset(images[generateRandom()]),
+          ElevatedButton(
+            onPressed: () {
+              int value = generateRandom();
+              setState(() {
+                Image.asset(images[value]);
+              });
+            },
+            child: const Text("Click!"),
+          ),
+        ],
       ),
     );
   }
